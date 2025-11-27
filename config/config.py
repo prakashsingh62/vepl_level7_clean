22e44f4705555a36ca86a03819c70a29022eaf44
prakashsingh62 <prakashkumarsing@gmail.com>
2025-11-27 11:58:02 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,6 +2,10 @@ CONFIG = {
     "sheet_id": "1hKMwlnN3GAE4dxVGvq2WHT2-Om9SJ3P91L8cxioAeoo",
     "sheet_tab_name": "RFQ TEST SHEET",
     "credentials_file": "service_account.json",
-    "gmail_query": "from:prakash.ventil1@gmail.com newer_than:5d",
+    
+    # Completely disable Gmail reader for Level-7 testing
+    "gmail_query": "",
+    "use_gmail_reader": False,
+    
     "mark_read_after_processing": False
 }
